0acee00c739537a3ddca94529661451a8b070dda
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-05 20:48:23 +0800

    another refactor

--- injector/src/lib.rs
+++ injector/src/lib.rs
@@ -1,1 +1,61 @@
+use std::ffi::CStr;
+use std::mem::zeroed;
+use windows::Win32::Foundation::CloseHandle;
+use windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS};
+
 pub const E4RTHBYTE_SHELLCODE: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/e4rthbyte_shellcode.bin"));
+
+#[macro_export]
+macro_rules! msg {
+    ($($arg:tt)*) => {{
+        let msg = format!($($arg)*);
+        println!("[+] {}", msg);
+    }};
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {{
+        let msg = format!($($arg)*);
+        println!("[-] {}, exiting in 3s", msg);
+        std::thread::sleep(std::time::Duration::from_secs(3));
+        std::process::exit(1);
+    }};
+}
+
+pub fn get_pid<S>(process: S) -> Option<u32>
+where 
+    S: AsRef<str>
+{
+    unsafe {
+        let Ok(h_snapshot) = CreateToolhelp32Snapshot(
+            TH32CS_SNAPPROCESS,
+            0
+        ) else {
+            return None
+        };
+
+        let mut pe = PROCESSENTRY32 {
+            dwSize: size_of::<PROCESSENTRY32>() as _,
+            ..zeroed()
+        };
+
+        let mut success = Process32First(h_snapshot, &mut pe);
+
+        while success.is_ok() {
+            let exe_name = CStr::from_ptr(pe.szExeFile.as_ptr())
+                .to_string_lossy();
+
+            if exe_name.eq_ignore_ascii_case(process.as_ref()) {
+                let _ = CloseHandle(h_snapshot);
+                return Some(pe.th32ProcessID);
+            }
+
+            success = Process32Next(h_snapshot, &mut pe);
+        }
+
+        let _ = CloseHandle(h_snapshot);
+
+        None
+    }
+}
--- injector/src/main.rs
+++ injector/src/main.rs
@@ -1,70 +1,14 @@
+use injector::{error, get_pid, msg, E4RTHBYTE_SHELLCODE};
 use std::ffi::{c_void, CStr};
 use std::mem::{transmute, zeroed};
-use std::process::exit;
-use std::thread::sleep;
-use std::time::Duration;
-use injector::E4RTHBYTE_SHELLCODE;
-use windows::{
-    Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS},
-    Win32::System::Diagnostics::Debug::{WriteProcessMemory},
-    Win32::Foundation::{CloseHandle, FALSE},
-    Win32::System::Threading::{OpenProcess, CreateRemoteThread, LPTHREAD_START_ROUTINE, PROCESS_ALL_ACCESS},
-    Win32::System::Memory::{VirtualAllocEx, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE}
-};
-
-macro_rules! msg {
-    ($($arg:tt)*) => {{
-        let msg = format!($($arg)*);
-        println!("[+] {}", msg);
-    }};
-}
-
-macro_rules! error {
-    ($($arg:tt)*) => {{
-        let msg = format!($($arg)*);
-        println!("[-] {}, exiting in 3s", msg);
-        sleep(Duration::from_secs(3));
-        exit(1);
-    }};
-}
-
-fn get_roblox_pid() -> Option<u32> {
-    unsafe {
-        let Ok(h_snapshot) = CreateToolhelp32Snapshot(
-            TH32CS_SNAPPROCESS,
-            0
-        ) else {
-            return None
-        };
-
-        let mut pe = PROCESSENTRY32 {
-            dwSize: size_of::<PROCESSENTRY32>() as _,
-            ..zeroed()
-        };
-
-        let mut success = Process32First(h_snapshot, &mut pe);
-
-        while success.is_ok() {
-            let exe_name = CStr::from_ptr(pe.szExeFile.as_ptr())
-                .to_string_lossy();
-            
-            if exe_name.eq_ignore_ascii_case("RobloxStudioBeta.exe") {
-                let _ = CloseHandle(h_snapshot);
-                return Some(pe.th32ProcessID);
-            }
-
-            success = Process32Next(h_snapshot, &mut pe);
-        }
-
-        let _ = CloseHandle(h_snapshot);
-
-        None
-    }
-}
+use windows::Win32::Foundation::{CloseHandle, FALSE};
+use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
+use windows::Win32::System::Threading::{CreateRemoteThread, OpenProcess, LPTHREAD_START_ROUTINE, PROCESS_ALL_ACCESS};
+use windows::Win32::System::Memory::{VirtualAllocEx, MEM_COMMIT, MEM_RESERVE, PAGE_EXECUTE_READWRITE};
 
 fn main() {
     unsafe {
-        let Some(roblox_pid) = get_roblox_pid() else {
+        let Some(roblox_pid) = get_pid("RobloxStudioBeta.exe") else {
             error!("Roblox not found!");
         };
 
@@ -86,7 +30,8 @@ fn main() {
         let address = VirtualAllocEx(
             h_process, None, buffer_size,
             MEM_COMMIT | MEM_RESERVE,
-            PAGE_EXECUTE_READWRITE);
+            PAGE_EXECUTE_READWRITE
+        );
 
         if address.is_null() {
             let _ = CloseHandle(h_process);
@@ -106,9 +51,12 @@ fn main() {
         msg!("Memory wrote!");
 
         if CreateRemoteThread(
-            h_process, None, 0,
+            h_process, None, 
+            0,
             transmute::<*mut c_void, LPTHREAD_START_ROUTINE>(address),
-            None, 0, None
+            None, 
+            0, 
+            None
         ).is_err() {
             let _ = CloseHandle(h_process);
             error!("Failed to create remote thread");
