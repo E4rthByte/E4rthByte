bf64cdd405397c8a578cca89e8e72d7c8c8efe0a
admin <waypress@femboy.mom>
2025-06-05 17:06:58 +0500

    Print implementation

--- e4rthbyte/src/lib.rs
+++ e4rthbyte/src/lib.rs
@@ -1,16 +1,10 @@
+mod rbxapi;
+
 use std::ptr::null_mut;
-use windows_sys::Win32::UI::WindowsAndMessaging::{
-    MessageBoxA, MB_OK,
-};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn entrypoint() {
     unsafe {
-        MessageBoxA(
-            null_mut(),
-            b"E4rthByte!\0".as_ptr() as _,
-            b"Injected!!\0".as_ptr() as _,
-            MB_OK,
-        );
+        rbxapi::print(0, "FEMBOY GANG!");
     }
 }
