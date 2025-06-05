2c8f6fb876f8c7d1f91cc1d691932bea1629f687
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-05 17:09:43 +0800

    simple dll

--- e4rthbyte/src/lib.rs
+++ e4rthbyte/src/lib.rs
@@ -1,4 +1,16 @@
+use std::ptr::null_mut;
+use windows_sys::Win32::UI::WindowsAndMessaging::{
+    MessageBoxA, MB_OK,
+};
+
 #[unsafe(no_mangle)]
 pub extern "C" fn entrypoint() {
-    println!("Hello, World!")
+    unsafe {
+        MessageBoxA(
+            null_mut(),
+            b"E4rthByte!\0".as_ptr() as _,
+            b"Injected!!\0".as_ptr() as _,
+            MB_OK,
+        );
+    }
 }
