48b3841e0dbf5d36235cfc810fc092c97a2c86e3
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-06 22:07:35 +0800

    feat unimplemented tokio

--- e4rthbyte/src/lib.rs
+++ e4rthbyte/src/lib.rs
@@ -1,18 +1,30 @@
 use rbxapi::api::{print, rbx_getscheduler};
 
-use std::panic;
+use std::{panic, thread};
+use tokio::runtime::Builder;
+use tokio::time::{sleep, Duration};
 use panichook::messagebox_panic_hook;
 
-fn main() {
+async fn main() {
     print(0, "FEMBOY GANG");
-
-    let scheduler = rbx_getscheduler();
-
-    print(0, scheduler.to_string().as_str());
+    
+    // loop {
+        let scheduler = rbx_getscheduler();
+        print(0, scheduler.to_string().as_str());
+        
+        // sleep(Duration::from_millis(500)).await;
+    // }
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn entrypoint() {
     panic::set_hook(Box::new(|panic_info| messagebox_panic_hook(panic_info)));
-    main();
+    
+    let rt = Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("Failed to build Tokio runtime");
+    
+    // TODO: run it in the new thread. The std::thread::spawn causes roblox freeze and crash
+    rt.block_on(main());
 }
