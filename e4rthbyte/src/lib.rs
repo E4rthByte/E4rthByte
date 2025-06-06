66464e90a6db82f5a2fa38a0cea13fd4ca72fe60
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-06-06 22:30:06 +0800

    use smol.

--- e4rthbyte/src/lib.rs
+++ e4rthbyte/src/lib.rs
@@ -1,30 +1,37 @@
+#![feature(future_join)]
+
+use std::future::join;
 use rbxapi::api::{print, rbx_getscheduler};
 
 use std::panic;
-use tokio::runtime::Builder;
-use tokio::time::{sleep, Duration};
+use std::time::Duration;
+use smol::Timer;
 use panichook::messagebox_panic_hook;
 
-async fn main() {
-    print(0, "FEMBOY GANG");
-    
+async fn scheduler() {
     loop {
         let scheduler = rbx_getscheduler();
-        print(0, scheduler.to_string().as_str());
-        
-        sleep(Duration::from_millis(500)).await;
+        print(0, format!("scheduler: {}", scheduler).as_str());
+        Timer::after(Duration::from_millis(50)).await;
     }
+}
+
+async fn print_gang() {
+    loop {
+        print(0, "FEMBOY GANG");
+        Timer::after(Duration::from_millis(600)).await;
+    }
+}
+
+async fn main() {
+    join!(scheduler(), print_gang()).await;
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn entrypoint() {
     panic::set_hook(Box::new(|panic_info| messagebox_panic_hook(panic_info)));
     
-    let rt = Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .expect("Failed to build Tokio runtime");
-    
-    // TODO: run it in the new thread. The std::thread::spawn causes roblox freeze and crash
-    rt.block_on(main());
+    smol::block_on(async { 
+        main().await
+    });
 }
